a8ebf185817554c42edc071980b704314ad7c48a
jwwylie1 <jwwylie1@gmail.com>
2025-08-08 13:44:15 -0400

    change db uri

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,15 +4,18 @@ export const signup = async (req, res) => {
 	const {email, password, name} = req.body;
 	const userExists = await User.findOne({ email });
 
-	if (userExists) {
-		return res.status(400).json({message: "User already exists"});
+	try {
+		if (userExists) {
+			return res.status(400).json({message: "User already exists"});
+		}
+		const user = await User.create({ name, email, password });
+
+		// sent
+		res.status(201).json({user, message: "User created successfully" });
+	} catch (error) {
+		res.send(500).json({ message: error.message });
 	}
-	const user = await User.create({ name, email, password });
 
-	// sent
-	res.status(201).json({user, message: "User created successfully" });
-
-	res.send("Sign up route called")
 }
 
 export const login = async (req, res) => {
